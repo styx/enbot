e3a723c014ac0cf96fcf526074c3f98aa45b3ec9
Mikhail S. Pobolovets <styx.mp@gmail.com>
2016-07-05 16:48:22 +0300

    Add Nadia configuration

--- mix.exs
+++ mix.exs
@@ -10,23 +10,17 @@ defmodule Enbot.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
+  # Type "mix help compile.app" for information
   def application do
-    [applications: [:logger]]
+    [
+      applications: [:logger, :nadia],
+    ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  # Type "mix help deps" for examples and options
   defp deps do
-     [{:nadia, "~> 0.3"}]
+     [
+       {:nadia, "~> 0.3"},
+     ]
   end
 end
